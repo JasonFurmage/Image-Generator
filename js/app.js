8ae2c1f5495d49a4fe7a32a212c64de78909c455
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-02-05 20:24:12 +0000

    Added title and user interface elements.
    Added select element for email addresses.
    Added text field for inputing new email address.
    Added buttons for canceling and adding.

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const contentEL = document.getElementById('container');
+const selectEL = document.getElementById('accounts');
+
+selectEL.addEventListener('change', function() {
+    let selectedValue = selectEL.value;
+});
